eed0d2003b683c25a7c4af9200d1d81ecf2329a2
Hasnain-MarkCoders <hasnain.raza@markcoders.com>
2025-07-24 05:07:21 +0500

    unused imports removed

--- src/app.js
+++ src/app.js
@@ -1,14 +1,6 @@
 import * as THREE from 'three'
 import { addPass, useCamera, useGui, useRenderSize, useScene, useTick } from './render/init.js'
-// import postprocessing passes
-import { SavePass } from 'three/examples/jsm/postprocessing/SavePass.js'
-import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
-import { BlendShader } from 'three/examples/jsm/shaders/BlendShader.js'
-import { CopyShader } from 'three/examples/jsm/shaders/CopyShader.js'
 import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
-
-import vertexShader from './shaders/vertex.glsl'
-import fragmentShader from './shaders/fragment.glsl'
 
 import vertexPars from './shaders/vertex_pars.glsl'
 import vertexMain from './shaders/vertex_main.glsl'
@@ -21,16 +13,12 @@ const startApp = () => {
   const gui = useGui()
   const { width, height } = useRenderSize()
 
-  // settings
-  const MOTION_BLUR_AMOUNT = 0.725
-
   // lighting
   const dirLight = new THREE.DirectionalLight('#526cff', 0.6)
   dirLight.position.set(2, 2, 2)
 
   const ambientLight = new THREE.AmbientLight('#4255ff', 0.5)
   scene.add(dirLight, ambientLight)
-
 
   // meshes
   const geometry = new THREE.IcosahedronGeometry(1, 400)
@@ -76,17 +64,9 @@ const startApp = () => {
   cameraFolder.open()
 
   // postprocessing
-  const renderTargetParameters = {
-    minFilter: THREE.LinearFilter,
-    magFilter: THREE.LinearFilter,
-    stencilBuffer: false,
-  }
-
-
-  // postprocessing
   addPass(new UnrealBloomPass(new THREE.Vector2(width, height), 0.7, 0.4, 0.4))
 
-  useTick(({ timestamp, timeDiff }) => {
+  useTick(({ timestamp }) => {
     const time = timestamp / 5000
     material.userData.shader.uniforms.uTime.value = time
   })
